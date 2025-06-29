6c86f397c4a43c16a22e3547adbc7a30dc19a5eb
xiaokouxyz <91874335+xiaokouxyz@users.noreply.github.com>
2025-06-29 14:14:47 +0800

    feat: 11-吃上细糠，升级SpringAI框架

---
+++ ai-mcp-knowledge-app/src/main/java/com/kou/knowledge/Application.java
@@ -0,0 +1,17 @@
+package com.kou.knowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author KouJY
+ * @description
+ * @create 2025-06-29 10:35
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
